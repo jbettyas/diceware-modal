d69e9de9d35a634033eeab7fa9a6ac80f10b428d
Jason Bettyas <jbettyas@gmail.com>
2019-05-10 15:57:22 -0700

    More content & reset button

--- src/App.js
+++ src/App.js
@@ -2,19 +2,63 @@ import React, { Component } from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 import './App.css';
 import { WORDS } from './shared/words';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, Container } from "mdbreact";
 
 class Header extends Component {
 
   render(){
     return(
-      <div>
-        <Jumbotron>
+      <div className="jumbo">
+        <Jumbotron id="jumbo" className="jumbo">
           <h1 className="display-3">Diceware</h1>
-          <p className="lead">Let's get a new password!</p>
+          <p className="lead">Let's generate a more secure password!</p>
         </Jumbotron>
       </div>
     )
   }
+}
+
+const Footer = () => {
+
+  return (
+    <Container>
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Footer Content</h5>
+            <p>
+            I followed the example of <a href="http://www.dicewarepasswords.com/" target="_blank" rel="noopener noreferrer">Mani Modi</a>. She put together a great site full of information! 
+              Check out her About page and buy a password from her if you want one. This site was put together as a ReactJS portfolio project.
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Resources</h5>
+            <ul>
+              <li>
+                <a href="http://www.dicewarepasswords.com/" target="_blank" rel="noopener noreferrer">Mani Modi</a>
+              </li>
+              <li>
+                <a href="www.diceware.com/" target="_blank" rel="noopener noreferrer">Diceware Passphrase Home Page</a>
+              </li>
+              <li>
+                <a href="https://en.wikipedia.org/wiki/Diceware" target="_blank" rel="noopener noreferrer">Diceware Wiki</a>
+              </li>
+              <li>
+                <a href="https://github.com/jbettyas/diceware" target="_blank" rel="noopener noreferrer">github</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="http://www.projecthep.com"> ProjectHep.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+    </Container>
+  );
 }
 
 class App extends Component {
@@ -22,17 +66,20 @@ class App extends Component {
     super(props);
     this.state = {
       words: WORDS,
+      dropdownOpen: false,
       finalCodes: []
     };
+
   }
 
   generateNumbers() {
+
     this.setState({ finalCodes: [] });
     var wordCodes = [];
     var storeNumbers = [];
     var finalPass = [];
 
-    for(var j=1; j<7; j++){
+    for(var j=1; j<6 + 1; j++){
 
       for(var i=1; i<6; i++){
 
@@ -41,7 +88,6 @@ class App extends Component {
       }
 
       wordCodes.push(storeNumbers.join(''));
-      //this.state.finalCodes.push(storeNumbers.join(''));
 
       console.log(wordCodes);
 
@@ -62,28 +108,45 @@ class App extends Component {
     this.setState({ finalCodes: finalPass });
   }
 
+  resetValues() {
+    this.setState({ finalCodes: [] })
+  }
+
   render() {
 
     console.log(this.state.finalCodes.join(' '));
 
-    var passContent = this.state.finalCodes.join('') !== "" ? <p>Your new password is: </p> : null;
+    var passOneContent = this.state.finalCodes.join('') !== "" ? <p className="lead"><strong>Your new password is:</strong></p> : null;
+    var passAllContent = this.state.finalCodes.join('') !== "" ? <p className="lead"><strong>The words used in the password are:</strong></p> : null;
 
     return (
       <div className="App">
         <Header />
-        <h4 className="lead">Click the button below to create a new password</h4><br/>
-        <Button color="danger" onClick={() => this.generateNumbers()}>Generate Password</Button>
+        <Container>
+          <p className="lead">Diceware is a great way to create a secure password with lots of entropy. You can find a lot more information about Diceware 
+          listed in the footer at the bottom of the page. This password generator uses six words to build your new, random password.</p>
+          <h4 className="lead"><strong>Click the button below to create a new password</strong></h4><br/>
+
+          <Button color="success" onClick={() => this.generateNumbers()}>Generate Password</Button>
+
+          <br/><br/>
+
+          <div>
+            {passOneContent}
+            <h3 className="lead" id="visible-input">{this.state.finalCodes.join('')}</h3>  
+          </div>
+          <br/>
+          <div>
+            {passAllContent}
+            <h3 className="lead">{this.state.finalCodes.join(' ')}</h3>
+          </div>
+          <br/>
+          <Button color="danger" onClick={() => this.resetValues()}>Reset</Button>
+
+
+        </Container>
         <br/><br/>
-        <div>
-        {passContent}
-            <h3 className="lead" id="visible-input">{this.state.finalCodes.join('')}</h3>
-            
-        </div>
-        <br/>
-        <div>
-          <h3 className="lead">{this.state.finalCodes.join(' ')}</h3>
-        </div>
-        
+        <Footer />        
       </div>
     );
   }
